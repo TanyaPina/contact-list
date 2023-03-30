110baf6e7d1b9645e9402d4533aad700cb390a3f
Tanya Pina <tanyapina15@gmail.com>
2023-03-30 13:47:11 -0700

    added contacts component

---
+++ client/src/components/Contacts.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import * as ioicons from 'react-icons/io5'
+import MyForm from './Form';
+
+const ListContacts = () => {
+
+}
+
+
+export default ListContacts
