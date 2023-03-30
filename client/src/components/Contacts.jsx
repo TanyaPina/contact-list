5acee120e5316370be2ee7a8f93ca381bc2d0bf7
Tanya Pina <tanyapina15@gmail.com>
2023-03-30 13:50:31 -0700

    set states for contacts component

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -4,7 +4,10 @@ import MyForm from './Form';
 
 const ListContacts = () => {
 
+    // this is my original state with an array of contacts
+    const [contacts, setContacts] = useState([]);
 }
-
-
+    //this is the state needed for the UpdateRequest
+    const [editingContact, setEditingSContact] = useState(null)
+    
 export default ListContacts
