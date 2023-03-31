2bd0fd00651bc65a8c7dacb692701b46b1d19a76
Tanya Pina <tanyapina15@gmail.com>
2023-03-30 23:17:30 -0700

    added function to save new contacts

--- client/src/components/Contacts.jsx
+++ client/src/components/Contacts.jsx
@@ -9,5 +9,23 @@ const ListContacts = () => {
 }
     //this is the state needed for the UpdateRequest
     const [editingContact, setEditingSContact] = useState(null)
+
+    const loadContacts = () => {
+        // A function to fetch the list of contacts that will be load anytime that list changes
+        fetch("http://localhost:8080/api/contacts")
+            .then((response) => response.json())
+            .then((contacts) => {
+                setContacts(contacts);
+            });
+    }
+
+    useEffect(() => {
+        loadContacts();
+    }, [contacts]);
+
+    const onSaveContact = (newContact) => {
+        //console.log(newStudent, "From the parent - List of Students");
+        setContacts((contacts) => [...contacts, newContacts]);
+    }
     
 export default ListContacts
